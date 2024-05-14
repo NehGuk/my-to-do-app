a9e2762149221f8e85690456f7ba074f7e724021
NehGuk <nurture.tumulus_0n@icloud.com>
2024-05-15 00:55:26 +0200

    Adds todo_list and first sample todos

---
+++ app.py
@@ -0,0 +1,13 @@
+print("Hi there!\nMy first Python program.")
+numberTest = 10
+print(numberTest)
+
+todo_list = []
+todo_list.append("Buy milk")
+todo_list.append("Learn more about Azure")
+todo_list.append("Complete first Python project")
+
+for todo in todo_list:
+    print(todo)
+
+    
